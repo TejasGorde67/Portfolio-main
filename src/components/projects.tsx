36db89307d0991349b4ff3e31decfffda4f94221
Tejas Gorde <tejasgorde67@gmail.com>
2025-02-27 19:45:45 +0530

    minor designing changes

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,9 +1,16 @@
 // components/MyProjects.tsx
-'use client';
-import React, { useState } from 'react';
-import { FaFolderOpen, FaReact, FaNodeJs, FaDatabase,FaWeebly,FaMobileAlt } from 'react-icons/fa';
-import { SiTypescript, SiNextdotjs, SiTailwindcss } from 'react-icons/si';
-import LoadMoreButton from './loadmore';
+"use client";
+import React, { useState } from "react";
+import {
+  FaFolderOpen,
+  FaReact,
+  FaNodeJs,
+  FaDatabase,
+  FaWeebly,
+  FaMobileAlt,
+} from "react-icons/fa";
+import { SiTypescript, SiNextdotjs, SiTailwindcss } from "react-icons/si";
+import LoadMoreButton from "./loadmore";
 
 interface Project {
   title: string;
@@ -16,7 +23,10 @@ interface MyProjectsProps {
   showLoadMore?: boolean;
 }
 
-const MyProjects: React.FC<MyProjectsProps> = ({ projects, showLoadMore = false }) => {
+const MyProjects: React.FC<MyProjectsProps> = ({
+  projects,
+  showLoadMore = false,
+}) => {
   const [visibleProjects, setVisibleProjects] = useState(projects.length);
 
   const handleLoadMore = () => {
@@ -24,46 +34,38 @@ const MyProjects: React.FC<MyProjectsProps> = ({ projects, showLoadMore = false 
   };
 
   return (
-    <div className='mt-20'>
-      <div className='flex justify-center items-center'>
-        <FaFolderOpen className='text-2xl mr-2' />
-        <h1 className='text-center'> | My Projects |</h1>
+    <div className="mt-20">
+      <div className="flex justify-center items-center mb-20">
+        <FaFolderOpen className="text-2xl mr-2" />
+        <h1 className="text-center"> | My Projects |</h1>
       </div>
 
-     
-
-
-      <div className='grid grid-cols-1 sm:grid-cols-2 gap-y-5 justify-items-center px-2 py-2 mt-10'>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-5 justify-items-center px-2 py-2 mt-10">
         {projects.slice(0, visibleProjects).map((project, index) => (
-          <div key={index} className='bg-white dark:bg-transparent p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 border border-b;lue-700'>
-
-            <div className='flex items-center mb-4'>
-              <div className='text-4xl mr-4 transition-transform duration-300 hover:scale-150'>
+          <div
+            key={index}
+            className="bg-white dark:bg-transparent p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105 border border-b;lue-700"
+          >
+            <div className="flex items-center mb-4">
+              <div className="text-4xl mr-4 transition-transform duration-300 hover:scale-150">
                 {project.icon}
               </div>
               <div>
-                <h2 className='text-xl font-bold text-gray-800 dark:text-white'>{project.title}</h2>
-                <p className='text-sm text-gray-600 dark:text-gray-300'>{project.description}</p>
+                <h2 className="text-xl font-bold text-gray-800 dark:text-white">
+                  {project.title}
+                </h2>
+                <p className="text-sm text-gray-600 dark:text-gray-300">
+                  {project.description}
+                </p>
               </div>
             </div>
           </div>
         ))}
       </div>
-      <div className='flex justify-center mt-2'>
-      <LoadMoreButton />
-     </div>
-
-     
-
-      
-
-
-
-
+      <div className="flex justify-center mt-2">
+        <LoadMoreButton />
+      </div>
     </div>
-
-
-    
   );
 };
 
