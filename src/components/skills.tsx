4dd10a154bb25cf2c049770bf43e7c9846a621d4
Tejas Gorde <tejasgorde67@gmail.com>
2025-02-28 13:36:57 +0530

    added vercel & github img

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -7,66 +7,76 @@ import {
   TooltipTrigger,
 } from "./ui/tooltip";
 import Image from "next/image";
+import { useDarkMode } from "../hooks/useDarkMode";
 import { FaTools } from "react-icons/fa";
+
 interface ImageData {
   src: string;
   alt: string;
   tooltip: string;
 }
 
-const images: ImageData[] = [
-  { src: "/assests/skills/css.webp", alt: "CSS", tooltip: "CSS" },
-  { src: "/assests/skills/docker.webp", alt: "Docker", tooltip: "Docker" },
-  { src: "/assests/skills/fastapi.webp", alt: "FastAPI", tooltip: "FastAPI" },
-  { src: "/assests/skills/git.webp", alt: "Git", tooltip: "Git" },
-  { src: "/assests/skills/github.webp", alt: "GitHub", tooltip: "GitHub" },
-  {
-    src: "/assests/skills/githubActions.webp",
-    alt: "GitHub Actions",
-    tooltip: "GitHub Actions",
-  },
-  { src: "/assests/skills/gitlab.webp", alt: "GitLab", tooltip: "GitLab" },
-  { src: "/assests/skills/html.webp", alt: "HTML", tooltip: "HTML" },
-  { src: "/assests/skills/js.webp", alt: "JavaScript", tooltip: "JavaScript" },
-  { src: "/assests/skills/mongoDB.webp", alt: "MongoDB", tooltip: "MongoDB" },
-  { src: "/assests/skills/mysql.webp", alt: "MySQL", tooltip: "MySQL" },
-  { src: "/assests/skills/aws.webp", alt: "AWS", tooltip: "AWS" },
-  { src: "/assests/skills/next.webp", alt: "Next.js", tooltip: "Next.js" },
-  { src: "/assests/skills/nodejs.webp", alt: "Node.js", tooltip: "Node.js" },
-  {
-    src: "/assests/skills/postgre.webp",
-    alt: "PostgreSQL",
-    tooltip: "PostgreSQL",
-  },
-  { src: "/assests/skills/react.webp", alt: "React", tooltip: "React" },
-  { src: "/assests/skills/replit.webp", alt: "Replit", tooltip: "Replit" },
-  { src: "/assests/skills/sass.webp", alt: "Sass", tooltip: "Sass" },
-  {
-    src: "/assests/skills/typescript.webp",
-    alt: "TypeScript",
-    tooltip: "TypeScript",
-  },
-  { src: "/assests/skills/vercel.webp", alt: "Vercel", tooltip: "Vercel" },
-
-  { src: "/assests/skills/vite.webp", alt: "Vite", tooltip: "Vite" },
-  {
-    src: "/assests/skills/tailwind.webp",
-    alt: "Tailwind CSS",
-    tooltip: "Tailwind CSS",
-  },
-
-  { src: "/assests/skills/python.webp", alt: "Python", tooltip: "Python" },
-  { src: "/assests/skills/netlify.webp", alt: "Netlify", tooltip: "Netlify" },
-];
-
 const HoverImageComponent: React.FC = () => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const { isDarkMode } = useDarkMode();
+
+  const images: ImageData[] = [
+    { src: "/assests/skills/css.webp", alt: "CSS", tooltip: "CSS" },
+    { src: "/assests/skills/docker.webp", alt: "Docker", tooltip: "Docker" },
+    { src: "/assests/skills/fastapi.webp", alt: "FastAPI", tooltip: "FastAPI" },
+    { src: "/assests/skills/git.webp", alt: "Git", tooltip: "Git" },
+    { src: "/assests/skills/github.webp", alt: "GitHub", tooltip: "GitHub" },
+    {
+      src: "/assests/skills/githubActions.webp",
+      alt: "GitHub Actions",
+      tooltip: "GitHub Actions",
+    },
+    { src: "/assests/skills/gitlab.webp", alt: "GitLab", tooltip: "GitLab" },
+    { src: "/assests/skills/html.webp", alt: "HTML", tooltip: "HTML" },
+    {
+      src: "/assests/skills/js.webp",
+      alt: "JavaScript",
+      tooltip: "JavaScript",
+    },
+    { src: "/assests/skills/mongoDB.webp", alt: "MongoDB", tooltip: "MongoDB" },
+    { src: "/assests/skills/mysql.webp", alt: "MySQL", tooltip: "MySQL" },
+    { src: "/assests/skills/aws.webp", alt: "AWS", tooltip: "AWS" },
+    { src: "/assests/skills/next.webp", alt: "Next.js", tooltip: "Next.js" },
+    { src: "/assests/skills/nodejs.webp", alt: "Node.js", tooltip: "Node.js" },
+    {
+      src: "/assests/skills/postgre.webp",
+      alt: "PostgreSQL",
+      tooltip: "PostgreSQL",
+    },
+    { src: "/assests/skills/react.webp", alt: "React", tooltip: "React" },
+    { src: "/assests/skills/replit.webp", alt: "Replit", tooltip: "Replit" },
+    { src: "/assests/skills/sass.webp", alt: "Sass", tooltip: "Sass" },
+    {
+      src: "/assests/skills/typescript.webp",
+      alt: "TypeScript",
+      tooltip: "TypeScript",
+    },
+    {
+      src: isDarkMode
+        ? "/assests/skills/vercelwhite.webp"
+        : "/assests/skills/vercel.webp",
+      alt: "Vercel",
+      tooltip: "Vercel",
+    },
+    { src: "/assests/skills/vite.webp", alt: "Vite", tooltip: "Vite" },
+    {
+      src: "/assests/skills/tailwind.webp",
+      alt: "Tailwind CSS",
+      tooltip: "Tailwind CSS",
+    },
+    { src: "/assests/skills/python.webp", alt: "Python", tooltip: "Python" },
+    { src: "/assests/skills/netlify.webp", alt: "Netlify", tooltip: "Netlify" },
+  ];
 
   return (
     <TooltipProvider delayDuration={0}>
       <div className="mt-20">
         <div className="flex justify-center items-center">
-          {/* <FaTools className="text-2xl mr-2 text-blue-300" /> */}
           <h1 className="text-center"> 🛠️| My skills |</h1>
         </div>
         <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-y-14 gap-x-14 justify-items-center px-2 py-2 mt-10">
@@ -80,31 +90,33 @@ const HoverImageComponent: React.FC = () => {
                 >
                   <div
                     className={`transition-all duration-300 ease-in-out
-            ${
-              hoveredIndex === index ? "scale-125 z-10 rotate-360 " : "scale-80"
-            }
-            ${
-              hoveredIndex !== null && hoveredIndex !== index
-                ? "blur-sm scale-75"
-                : ""
-            }`}
+                      ${
+                        hoveredIndex === index
+                          ? "scale-125 z-10 rotate-360 "
+                          : "scale-80"
+                      }
+                      ${
+                        hoveredIndex !== null && hoveredIndex !== index
+                          ? "blur-sm scale-75"
+                          : ""
+                      }`}
                   >
-                    {image ? (
-                      <Image
-                        src={image.src}
-                        alt={image.alt}
-                        width={48}
-                        height={48}
-                        className="object-cover"
-                      />
-                    ) : (
-                      <div></div> // Placeholder for empty image
-                    )}
+                    <Image
+                      src={image.src}
+                      alt={image.alt}
+                      width={48}
+                      height={48}
+                      className="object-cover"
+                    />
                   </div>
                 </div>
               </TooltipTrigger>
-              {hoveredIndex === index && image && (
-                <TooltipContent className="p-2 text-sm bg-black text-white rounded">
+              {hoveredIndex === index && (
+                <TooltipContent
+                  className={`p-2 text-sm rounded ${
+                    isDarkMode ? "bg-white text-black" : "bg-black text-white"
+                  }`}
+                >
                   <p className="font-bold text-xl">{image.tooltip}</p>
                 </TooltipContent>
               )}
--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,14 +1,38 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// export const useDarkMode = () => {
+//   const [isDarkMode, setIsDarkMode] = useState(false);
+
+//   useEffect(() => {
+//     const darkModePreference = window.matchMedia(
+//       "(prefers-color-scheme: dark)"
+//     ).matches;
+//     setIsDarkMode(darkModePreference);
+//   }, []);
+
+//   return { isDarkMode, toggleDarkMode: () => setIsDarkMode(!isDarkMode) };
+// };
+
+import { useState, useEffect } from "react";
 
 export const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
+    if (typeof window !== "undefined") {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+    return false;
+  });
 
   useEffect(() => {
-    const darkModePreference = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    setIsDarkMode(darkModePreference);
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsDarkMode(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
   }, []);
 
-  return { isDarkMode, toggleDarkMode: () => setIsDarkMode(!isDarkMode) };
+  return { isDarkMode };
 };
