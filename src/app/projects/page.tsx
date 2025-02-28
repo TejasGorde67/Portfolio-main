24cf0d0340dc90616f16194fa7e1b1220ce45df9
Tejas Gorde <tejasgorde67@gmail.com>
2025-02-28 15:44:55 +0530

    Added project images

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -49,7 +49,7 @@ export default function Component() {
                     className="w-full mt-4"
                   >
                     <Image
-                      src=""
+                      src="/assests/cloudbrain.png"
                       height="1000"
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -71,7 +71,7 @@ export default function Component() {
                       translateZ={20}
                       translateX={40}
                       as="a"
-                      href=""
+                      href="https://github.com/TejasGorde67/AnonyGram"
                       className="flex items-center px-4 py-2 text-white text-xl font-bold"
                     >
                       <GitHubLogoIcon className="w-8 h-8" />
@@ -111,7 +111,7 @@ export default function Component() {
                     className="w-full mt-4"
                   >
                     <Image
-                      src=""
+                      src="/assests/cloudbrain.png"
                       height="1000"
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -132,7 +132,7 @@ export default function Component() {
                       translateZ={20}
                       translateX={40}
                       as="a"
-                      href=""
+                      href="https://github.com/TejasGorde67/CloudBrain"
                       className="flex items-center px-4 py-2 text-white text-xl font-bold"
                     >
                       <GitHubLogoIcon className="w-8 h-8" />
@@ -172,7 +172,7 @@ export default function Component() {
                     className="w-full mt-4"
                   >
                     <Image
-                      src=""
+                      src="/assests/pageflix.png"
                       height="1000"
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -194,7 +194,7 @@ export default function Component() {
                       translateZ={20}
                       translateX={40}
                       as="a"
-                      href=""
+                      href="https://github.com/TejasGorde67/PageFlix"
                       className="flex items-center px-4 py-2 text-white text-xl font-bold"
                     >
                       <GitHubLogoIcon className="w-8 h-8" />
@@ -235,8 +235,8 @@ export default function Component() {
                     className="w-full mt-4"
                   >
                     <Image
-                      // public\assests\careinsightimg1.png
-                      src=""
+                      // public\assests\sociallite.png
+                      src="/assests/socialite.png"
                       height="1000"
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -258,69 +258,7 @@ export default function Component() {
                       translateZ={20}
                       translateX={40}
                       as="a"
-                      href=""
-                      className="flex items-center px-4 py-2 text-white text-xl font-bold"
-                    >
-                      <GitHubLogoIcon className="w-8 h-8" />
-                    </CardItem>
-                  </div>
-                </CardBody>
-              </motion.div>
-            </CardContainer>
-            <CardContainer className="inter-var h-full">
-              {" "}
-              {/* Added h-full */}
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-              >
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-[550px] rounded-xl p-6 border">
-                  {" "}
-                  {/* Fixed height to 600px */}
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
-                  >
-                    Reltime Chat App
-                  </CardItem>
-                  <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                  >
-                    MERN, Socket.io, Tailwind CSS, Daisy UI, Zustand
-                  </CardItem>
-                  <CardItem
-                    translateZ="100"
-                    rotateX={0}
-                    rotateZ={-3}
-                    className="w-full mt-4"
-                  >
-                    <Image
-                      src=""
-                      height="1000"
-                      width="1000"
-                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                      alt="thumbnail"
-                    />
-                  </CardItem>
-                  <div className="flex justify-between items-center mt-20">
-                    <CardItem
-                      translateZ={20}
-                      translateX={-40}
-                      as="a"
-                      href=""
-                      className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    >
-                      Live Link →
-                    </CardItem>
-
-                    <CardItem
-                      translateZ={20}
-                      translateX={40}
-                      as="a"
-                      href=""
+                      href="https://github.com/TejasGorde67/Sociallite"
                       className="flex items-center px-4 py-2 text-white text-xl font-bold"
                     >
                       <GitHubLogoIcon className="w-8 h-8" />
