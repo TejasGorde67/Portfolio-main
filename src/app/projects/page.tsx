509dd8295dfa2d7714e10ee7c056377a9e5f14ad
Tejas Gorde <tejasgorde67@gmail.com>
2025-03-01 14:32:06 +0530

    added img for project section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -49,7 +49,7 @@ export default function Component() {
                     className="w-full mt-4"
                   >
                     <Image
-                      src="/assests/cloudbrain.png"
+                      src="/assests/anonygram.png"
                       height="1000"
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
