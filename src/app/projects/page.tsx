42fa25061ca2fb5b219abd260cc6eb10b07ad5ae
Tejas Gorde <tejasgorde67@gmail.com>
2025-03-07 23:54:23 +0530

    fixed typo error

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -218,7 +218,7 @@ export default function Component() {
                     translateZ="50"
                     className="text-xl font-bold text-neutral-600 dark:text-white"
                   >
-                    Sociallite
+                    Socialite
                   </CardItem>
                   <CardItem
                     as="p"
