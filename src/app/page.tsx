ec4a15d79ce0f9737e8cdd95033b1a224e02753f
Tejas Gorde <tejasgorde67@gmail.com>
2025-03-11 22:31:21 +0530

    made project titles clickable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,11 +51,13 @@ const projects = [
     title: "AnonyGram",
     description: "Anonymous social media platform.",
     icon: <FaComments className="text-green-400" />,
+    link: "#projects",
   },
   {
     title: "PageFlix",
     description: "Online Free Book browsing Platform.",
     icon: <FaTasks className="text-yellow-500" />,
+    link: "#projects",
   },
   {
     title: "Book Store App",
